1e53e7f9d497159938b1ee557a473bbf585a6e9b
unknown <arielpenna@gmail.com>
2021-12-09 01:54:22 -0300

    prueba heroku

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import './index.css'
-// import { Route } from 'react-router-dom'
 import Invitaciones from './Pages/Invitaciones'
 import './styles.css'
 
 function App() {
 	return (
 		<>
-			{/* <Route path='/'> */}
 			<Invitaciones />
-			{/* </Route> */}
 		</>
 	)
 }
